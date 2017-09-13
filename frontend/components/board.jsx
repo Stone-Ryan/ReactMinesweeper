8c5c108e4df229e2823d947e0bda9fbf5572b61b
SDRyan9 <stonedouglasryan@gmail.com>
2017-09-13 11:51:59 -0700

    Include minesweeper.js Lib and initialize board

---
+++ frontend/components/board.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    <h1>board</h1>
+  }
+}
+
+export default Board;
---
+++ frontend/components/game.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import * as Minesweeper from '../../minesweeper';
+
+import Board from './board';
+
+class Game extends Component {
+  constructor(props) {
+    super(props)
+
+    const board = new Minesweeper.Board(9, 10);
+
+    this.state = {
+      board,
+    }
+
+    this.updateGame = this.updateGame.bind(this);
+  }
+
+  updateGame() {
+
+  }
+
+  render() {
+    <Board board={this.state.board} update={this.updateGame} />
+  }
+}
+
+export default Game;
